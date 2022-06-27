45d628e5d99aa9b1154610a4e84eb5136b90df03
aoirint <aoirint@gmail.com>
2022-06-28 04:31:01 +0900

    post method

--- src/api/routers/site_aoirint_com.py
+++ src/api/routers/site_aoirint_com.py
@@ -4,6 +4,6 @@ from ..const import hmac_auth
 
 router = APIRouter()
 
-@router.get('/sync', dependencies=[Depends(hmac_auth)])
+@router.post('/sync', dependencies=[Depends(hmac_auth)])
 async def sync(body: Dict[str, Any]):
   print(body)
